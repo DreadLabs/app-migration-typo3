90402f5d03de0ba631e7795b4151cd9ee2f7df71
Thomas Juhnke <tommy@van-tomas.de>
2015-09-30 21:15:35 +0200

    [TASK] Add Name for NinjaMutex lock implementaiton

---
+++ Classes/Domain/Lock/Name.php
@@ -0,0 +1,34 @@
+<?php
+namespace DreadLabs\AppMigrationTypo3\Domain\Lock;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use DreadLabs\AppMigration\Lock\NinjaMutex\NameInterface;
+
+/**
+ * Name
+ *
+ * @author Thomas Juhnke <typo3@van-tomas.de>
+ */
+class Name implements NameInterface {
+
+	/**
+	 * Returns the name for the NinjaMutex lock
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		return 'typo3-cms-migration';
+	}
+}
